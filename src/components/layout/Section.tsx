4a60ca0d8c7feeaf2fa309bfb2b60ce714e4d198
iSaklov <ivansaklov@gmail.com>
2023-10-19 22:36:54 +0200

    refactor: updape mb in Section component

--- src/components/layout/Section.tsx
+++ src/components/layout/Section.tsx
@@ -14,10 +14,10 @@ export default function Section({
       id={id}
       className='container mx-auto my-20 min-h-screen max-w-7xl px-1'
     >
-      <h2 className='__heading-2 text-center'>{`${title.toLowerCase()}.`}</h2>
-      {subtitle && (
-        <h3 className='__heading-3 mb-20 text-center'>{subtitle}</h3>
-      )}
+      <div className='mb-20 text-center'>
+        <h2 className='__heading-2'>{`${title.toLowerCase()}.`}</h2>
+        {subtitle && <h3 className='__heading-3'>{subtitle}</h3>}
+      </div>
       {children}
     </section>
   )
