a40cc7394f2f8435ee7748b72192ca7f3b5e2809
iSaklov <ivansaklov@gmail.com>
2023-10-20 15:39:49 +0200

    refactor: makes adjustments to routing

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,3 +1,22 @@
-export default function Page({ params }: { params: { slug: string } }) {
-  return <div>My Post: {params.slug}</div>
+import { Suspense } from 'react'
+import ProjectHeader from '@/components/project/ProjectHeader'
+
+export default function Project({ params }: { params: { slug: string } }) {
+  return (
+    <div className='min-h-screen pt-80'>
+      {/* <Suspense fallback={<Loading />}> */}
+      <Suspense fallback={<p>Loading weather...</p>}>
+        <ProjectHeader name={params.slug} />
+      </Suspense>
+      <Suspense fallback={<p>Loading weather...</p>}>
+        {/* <ProjectImages /> */}
+      </Suspense>
+      <Suspense fallback={<p>Loading weather...</p>}>
+        {/* <ProjectLighthouse /> */}
+      </Suspense>
+      <Suspense fallback={<p>Loading weather...</p>}>
+        {/* <ProjectDetails /> */}
+      </Suspense>
+    </div>
+  )
 }
---
+++ src/components/project/ProjectHeader.tsx
@@ -0,0 +1,7 @@
+export default function ProjectHeader({ name }: { name: string }) {
+  return (
+    <div>
+      <h1 className='__heading-1'>{name}</h1>
+    </div>
+  )
+}
