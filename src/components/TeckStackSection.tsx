29052629bcec9c59c37a7a578b6d45c1bd9d02b3
iSaklov <ivansaklov@gmail.com>
2023-12-04 17:39:52 +0100

    feat: add tooltip in hover: to tech links

--- src/components/TeckStackSection.tsx
+++ src/components/TeckStackSection.tsx
@@ -12,14 +12,18 @@ export default async function TeckStackSection() {
 
   return (
     <Section id={idSection} title={title} subtitle={subtitle}>
-      <div className='flex flex-wrap items-center justify-center gap-8 pt-20 lg:gap-16'>
+      <div className='flex flex-wrap items-center justify-center gap-8 pt-20 md:gap-12 lg:gap-x-12 lg:gap-y-16'>
         {techs.map((tech) => (
-          <img
-            key={tech.id}
-            src={tech.icon[0].url}
-            alt={tech.name}
-            className='max-w-16 md:max-w-24 lg:max-w-28 aspect-square max-h-16 md:max-h-24 lg:max-h-28'
-          />
+          <div key={tech.id} className='group relative'>
+            <img
+              src={tech.icon[0].url}
+              alt={tech.name}
+              className='h-auto max-h-[64px] w-auto max-w-[64px] md:max-h-[80px] md:max-w-[80px] lg:max-w-[96px] lg:max-w-[96px] xl:max-h-[128px] xl:max-w-[128px]'
+            />
+            <span className='absolute bottom-0 left-1/2 z-10 hidden -translate-x-1/2 translate-y-1/2 scale-95 transform rounded-lg bg-primary-dark px-4 py-2 font-extralight text-white opacity-0 transition before:absolute before:-top-2 before:left-1/2 before:inline-block before:h-4 before:w-4 before:-translate-x-1/2 before:rotate-45 before:bg-primary-dark group-hover:translate-y-[130%] group-hover:scale-100 group-hover:opacity-100 lg:inline-block lg:text-[1.375rem]'>
+              {tech.name}
+            </span>
+          </div>
         ))}
       </div>
     </Section>
