b083abbac06843204a9281869bd50cadb8633ca1
iSaklov <ivansaklov@gmail.com>
2024-03-07 01:44:13 +0100

    refactor: fix Button component

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -16,14 +16,14 @@ export default function Button({
 }: ButtonProps) {
   const styles: Variants = {
     primary:
-      'bg-primary-dark text-white disabled:bg-light-gray disabled:text-mid-gray enabled:hover:bg-highlight-gray enabled:active:bg-accent-orange',
+      'bg-primary-dark text-white disabled:bg-light-gray disabled:text-mid-gray enabled:hover:bg-highlight-gray enabled:active:scale-95 transition',
     secondary:
-      'tracking-normal transition-all duration-300 ease-in-out before:relative before:block before:backface-hidden before:bottom-0 before:mx-auto before:mb-2 before:w-0 before:border before:border-solid before:border-primary-dark before:opacity-0 before:transition-all before:duration-300 before:ease-in-out after:relative after:block after:backface-hidden after:bottom-0 after:mx-auto after:mt-2 after:w-0 after:border after:border-solid after:border-primary-dark after:opacity-0 after:transition-all after:duration-300 after:ease-in-out enabled:active:tracking-widest enabled:active:before:-left-[10%] enabled:active:before:w-[120%] enabled:active:before:opacity-100 enabled:active:after:-left-[10%] enabled:active:after:w-[120%] enabled:active:after:opacity-100 md:enabled:hover:tracking-widest md:enabled:hover:before:-left-[10%] md:enabled:hover:before:w-[120%] md:enabled:hover:before:opacity-100 md:enabled:hover:after:-left-[10%] md:enabled:hover:after:w-[120%] md:enabled:hover:after:opacity-100'
+      'ease-[cubic-bezier(0.25,0.8,0.25,1)] duration-[400ms] tracking-normal transition-all enabled:active:scale-90 enabled:active:tracking-widest enabled:hover:tracking-widest before:pointer-events-none before:top-0 before:absolute before:h-px before:bg-primary-dark before:block before:w-0 before:left-1/2 enabled:before:hover:w-full enabled:before:hover:left-0 after:pointer-events-none after:bottom-0 after:absolute after:h-px after:bg-primary-dark after:block after:w-0 after:left-1/2 enabled:after:hover:w-full enabled:after:hover:left-0 before:ease-[cubic-bezier(0.25,0.8,0.25,1)] after:ease-[cubic-bezier(0.25,0.8,0.25,1)] after:duration-[400ms] before:duration-[400ms] enabled:before:active:w-full enabled:before:active:left-0 enabled:after:active:w-full enabled:after:active:left-0'
   }
 
   return (
     <button
-      className={`relative w-full max-w-[240px] border border-transparent px-6 py-3 text-center text-base font-light disabled:pointer-events-none md:max-w-[260px] md:px-8 md:py-4 md:text-lg xl:max-w-[280px] xl:text-xl ${styles[variant]} ${className}`}
+      className={`relative max-w-60 select-none border-none bg-none px-12 py-3 text-base font-light text-inherit disabled:pointer-events-none md:max-w-64 md:text-lg xl:max-w-72 xl:text-xl ${styles[variant]} ${className}`}
       {...props}
     >
       {children}
